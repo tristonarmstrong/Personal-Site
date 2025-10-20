8d783a75865b161856a61a2fa83b88b6e9242e8c
Triston Armstrong <tarmstrong95@proton.me>
2025-10-20 14:37:12 -0600

    import water as suggested by vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { mount } from "kiru"
 import { App } from "./App"
 import "./global.css"
-import "../public/water.css"
+import "/water.css"
 
 mount(<App />, document.getElementById("app")!)
