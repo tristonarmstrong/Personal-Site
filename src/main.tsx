ad51d77a0d016daa8848c08bcc87ca94ef467312
Triston Armstrong <tarmstrong95@proton.me>
2025-10-20 14:40:26 -0600

    revert water import - vite lied to me

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { mount } from "kiru"
 import { App } from "./App"
 import "./global.css"
-import "/water.css"
+import "../public/water.css"
 
 mount(<App />, document.getElementById("app")!)
