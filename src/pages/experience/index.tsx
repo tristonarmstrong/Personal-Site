93d5f83d2a5e0812a33ced3f7516ca9e4b4b84ec
Triston Armstrong <tarmstrong95@proton.me>
2025-10-27 14:31:03 -0600

    add freelance work to experience

--- src/pages/experience/index.tsx
+++ src/pages/experience/index.tsx
@@ -1,12 +1,19 @@
 export default function Experience() {
   return (
     <main className="text-sm mt-10 flex flex-col gap-4" style={"view-transition-name: homepage"}>
-      <p>Companies I've worked at and maybe where I'm currently working if I can convince someone to give me money in exchange for code.</p>
+      <p>Companies I've worked at and maybe where I'm currently working if I can convince someone to give me money in exchange for some code.</p>
       <ul>
         <Work href="https://ventrahealth.com/" comp="Ventra Health" time="2023 - Pres" />
         <Work href="https://www.randstadusa.com/" comp="Randstad Technologies" time="2021 - 2023" />
         <Work href="https://damianoglobal.com/" comp="Damiano Global Corp." time="2021" />
         <Work href="" comp="Makers Ladder LLC" time="2020" />
+      </ul>
+      <div>
+        <h2 className={"font-bold"}>Freelance Work</h2>
+        <p className={"text-gray-400"}>My experience freelancing was cool! 10/10 would do again.</p>
+      </div>
+      <ul>
+        <Work href="https://www.upwork.com/freelancers/~018467e8cbe2f71382" comp="Upwork" time="$40,000+" />
       </ul>
     </main>
   )
