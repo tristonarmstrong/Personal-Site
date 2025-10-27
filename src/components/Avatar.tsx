23876dbe2a7aa297b5a357c502019d665ce19081
Triston Armstrong <tarmstrong95@proton.me>
2025-10-27 11:02:03 -0600

    add view transition stuff to avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -2,6 +2,6 @@ import photo from '../assets/avatar.jpg'
 
 export function Avatar() {
   return (
-    <div className="w-10 h-10 rounded-full bg-center bg-cover" style={`background-image: url(${photo})`}></div>
+    <div style={`view-transition-name: avatar; background-image: url(${photo})`} className="w-10 h-10 rounded-full bg-center bg-cover"></div>
   )
 }
