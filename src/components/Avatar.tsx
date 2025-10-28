bdb342ad7c73bd81092766e5c9086bf3804e3f50
Triston Armstrong <tarmstrong95@proton.me>
2025-10-28 12:01:24 -0600

    move avatar to public

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,7 +1,8 @@
-import photo from '../assets/avatar.jpg'
-
 export function Avatar() {
   return (
-    <div style={`view-transition-name: avatar; background-image: url(${photo})`} className="w-10 h-10 rounded-full bg-center bg-cover"></div>
+    <div
+      style={`view-transition-name: avatar; background-image: url(/avatar.jpg)`}
+      className="w-10 h-10 rounded-full bg-center bg-cover">
+    </div>
   )
 }
