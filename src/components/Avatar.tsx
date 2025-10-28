e55e55c59202664b4aa55941295597ae58165732
Triston Armstrong <tarmstrong95@proton.me>
2025-10-28 12:50:12 -0600

    update avatar to webp

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,7 +1,7 @@
 export function Avatar() {
   return (
     <div
-      style={`view-transition-name: avatar; background-image: url(/avatar.jpg)`}
+      style={`view-transition-name: avatar; background-image: url(/avatar.webp)`}
       className="w-10 h-10 rounded-full bg-center bg-cover">
     </div>
   )
