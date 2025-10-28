fb087a8288fc88617492915d1265d30c6a381beb
Triston Armstrong <tarmstrong95@proton.me>
2025-10-27 21:30:53 -0600

    use transitions for avatar

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,48 +1,14 @@
-import { Link } from "kiru/router"
+import { Link, useFileRouter } from "kiru/router"
 import { Mail } from "../components/icons/Mail"
-import { useSignal, useViewTransition } from "kiru"
+import { useViewTransition } from "kiru"
 import { Avatar } from "../components/Avatar"
 
 export default function RootLayout({ children }: { children: JSX.Children }) {
-  const transition = useViewTransition()
-  const activeLink = useSignal(window.location.pathname)
-  const activeStyle = "border-b-1 border-yellow-500"
-
-  const handleNavigate = (route: string) => () => {
-    transition(() => activeLink.value = route)
-  }
-
-
   return (
     <div className="max-w-[500px] mx-auto mt-10 flex flex-col gap-4 px-2">
-      <nav className="flex justify-between items-center">
-        <Link style={"view-transition-name: nav"} to="/" onclick={handleNavigate("/")}><Avatar /></Link>
-        <div className="flex gap-3 [&>*]:hover:text-yellow-500">
-          <Link style={"view-transition-name: home"} to="/" onclick={handleNavigate("/")} className={activeLink.value == "/" ? activeStyle : ''}> Home</Link>
-          <Link style={"view-transition-name: experience"} to="/experience" onclick={handleNavigate("/experience")} className={activeLink.value == "/experience" ? activeStyle : ''}> Experience</Link>
-          <Link style={"view-transition-name: now"} to="/now" onclick={handleNavigate("/now")} className={activeLink.value == "/now" ? activeStyle : ''}> Now</Link>
-        </div>
-        <a href="#" onclick={(e) => {
-          e.preventDefault()
-          e.stopPropagation()
-          let a = document.createElement('a')
-          a.href = "mailto:triston95strong@gmail.com?subject=Reaching Out&body=Hey Triston, ...Put message here..."
-          a.click()
-        }}><Mail /></a>
-      </nav>
+      <Navigation />
 
       {children}
-      {/*
-      <Router transition>
-        <Route path="/" element={<Home />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/now" element={<Now />} />
-        <Route path="/blog" element={<Blogs />} fallthrough />
-        <Route path="/project" element={<Projects />} fallthrough />
-        <Route path="*" element={<h1>Oopsie! That page doesnt exist</h1>} />
-      </Router>
-
-    */}
 
       <footer className="text-center flex flex-col" style={"view-transition-name: foot"}>
         <hr className="opacity-50" />
@@ -50,5 +16,44 @@ export default function RootLayout({ children }: { children: JSX.Children }) {
         <small className="opacity-50">Made with ❤️ & 🍵</small>
       </footer>
     </div>
+  )
+}
+
+function Navigation() {
+  const { state } = useFileRouter()
+  const transition = useViewTransition()
+  const activeStyle = "border-b-1 border-yellow-500"
+
+  const handleNavigate = (route: string) => () => {
+    transition(() => state.path = route)
+  }
+
+  return (
+    <nav className="flex justify-between items-center">
+      <AutoHiddenAvatar handleNavigate={handleNavigate} />
+      <div className="flex gap-3 [&>*]:hover:text-yellow-500">
+        <Link style={"view-transition-name: home"} to="/" onclick={handleNavigate("/")} className={state.path == "/" ? activeStyle : ''}> Home</Link>
+        <Link style={"view-transition-name: experience"} to="/experience" onclick={handleNavigate("/experience")} className={state.path == "/experience" ? activeStyle : ''}> Experience</Link>
+        <Link style={"view-transition-name: now"} to="/now" onclick={handleNavigate("/now")} className={state.path == "/now" ? activeStyle : ''}> Now</Link>
+      </div>
+      <a href="#" onclick={(e) => {
+        e.preventDefault()
+        e.stopPropagation()
+        let a = document.createElement('a')
+        a.href = "mailto:triston95strong@gmail.com?subject=Reaching Out&body=Hey Triston, ...Put message here..."
+        a.click()
+      }}><Mail /></a>
+    </nav>
+
+  )
+}
+
+function AutoHiddenAvatar({ handleNavigate }: { handleNavigate: any }) {
+  const { state } = useFileRouter()
+
+  if (state.path.includes('blog')) return null
+
+  return (
+    <Link style={"view-transition-name: nav"} to="/" onclick={handleNavigate("/")}><Avatar /></Link>
   )
 }
