1a14b5c841e47215b1e42fd87155b0df18df3b65
Triston Armstrong <tarmstrong95@proton.me>
2025-10-31 08:49:45 -0600

    use link component in 404 page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from "kiru/router";
+
 export default function NotFoundPage() {
   return (
     <div className={"text-center"}>
       <h1 className={"text-[4rem] font-bold"}>404</h1>
       <h2 className={"text-lg"}>Where do you think you're going?</h2>
-      <a href={"/"} className={"text-yellow-500 underline"}>Go Home</a>
+      <Link to={"/"} className={"text-yellow-500 underline"}>Go Home</Link>
     </div>
   )
 }
