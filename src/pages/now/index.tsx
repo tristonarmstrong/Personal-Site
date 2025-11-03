02262b1addc1a5380fa3f189719278ec5bfa1caa
Triston Armstrong <tarmstrong95@proton.me>
2025-11-03 12:07:42 -0700

    add light theme support

--- src/pages/now/index.tsx
+++ src/pages/now/index.tsx
@@ -12,10 +12,10 @@ export default function Now() {
       <div>
         <div className={"mb-2"}>
           <h2 className={"text-xl"}>Projects</h2>
-          <p className={"text-gray-300"}>These are pojects that i actively work on. I have a graveyard too</p>
+          <p>These are pojects that i actively work on. I have a graveyard too</p>
         </div>
         <ul className={"mx-4"}>
-          <p className={"text-yellow-600"}>Web</p>
+          <h3 className={""}>Web</h3>
           {allProjects.filter(x => x.type === 'Web').map(x => {
             return (
               <Item label={x.title} href={`/project/${x.slug}`} transitionId={x.slug} />
@@ -24,7 +24,7 @@ export default function Now() {
           {/*
           <Item label="Homeschool Calendar" href="/project/homeschool-calendar" transitionId="homeschoolcalendar" />
           */}
-          <p className={"text-yellow-600 mt-2"}>Tool</p>
+          <h3 className={""}>Tool</h3>
           {allProjects.filter(x => x.type === 'Tool').map(x => {
             return (
               <Item label={x.title} href={`/project/${x.slug}`} transitionId={x.slug} />
@@ -36,18 +36,18 @@ export default function Now() {
       <div>
         <div className={"mb-2"}>
           <h2 className={"text-xl"}>OSS Contribs</h2>
-          <p className={"text-gray-300"}>I also enjoy contributing to open source projects when the opportunity presents itself! I've contributed to some, and a few that never made it through.</p>
+          <p>I also enjoy contributing to open source projects when the opportunity presents itself! I've contributed to some, and a few that never made it through.</p>
         </div>
         <div className={"flex flex-col gap-1"}>
           <ul className={"mx-4"}>
-            <p className={"text-yellow-600"}>Successful</p>
+            <h3 className={""}>Successful</h3>
             <Item label="BinCode" href="https://github.com/asadbek064/bincode/pull/1" />
             <Item label="Dissent" href="https://github.com/diamondburned/dissent/pull/371" />
             <Item label="Kiru" href="https://github.com/CrimsonChi/kiru" />
             <Item label="ChaiLauncher (Window Control)" href="https://github.com/Chai-Foundation/ChaiLauncher/pull/13" />
             <Item label="ChaiLauncher (Mod Loader)" href="https://github.com/Chai-Foundation/ChaiLauncher/pull/18" />
             <Item label="Chai-MCVM" href="https://github.com/tristanpoland/Chai-MCVM/pull/1" />
-            <p className={"text-yellow-600 mt-2"}>UnSuccessful</p>
+            <h3 className={""}>UnSuccessful</h3>
             <Item label="Omarchy (Flatpak Support)" href="https://github.com/basecamp/omarchy/issues/1881" />
             <Item label="Omarchy (Auto Fetch Icons)" href="https://github.com/basecamp/omarchy/pull/1905" />
             <Item label="Typescript" href="https://github.com/microsoft/TypeScript/pull/60269" />
@@ -59,7 +59,7 @@ export default function Now() {
       <div >
         <div className={"mb-2"}>
           <h2 className={"text-xl"}>Blogs</h2>
-          <p className={"text-gray-300"}>Sometimes I have knowledge I find worth sharing, and thus I write up a little something something from time to time</p>
+          <p>Sometimes I have knowledge I find worth sharing, and thus I write up a little something something from time to time</p>
         </div>
         <ul className={"mx-4"}>
           {allPosts.map(x => (
@@ -75,14 +75,14 @@ export default function Now() {
 function Item({ label, href, transitionId }: { label: string, href: string, transitionId?: string }) {
   if (href.includes("http")) {
     return (
-      <a href={href} className={href ? "text-gray-400 hover:text-yellow-500" : ""}>
+      <a href={href}>
         <LinkBody {...{ label, transitionId }} />
       </a>
     )
   }
 
   return (
-    <Link to={href} className={href ? "text-gray-400 hover:text-yellow-500" : ""}>
+    <Link to={href}>
       <LinkBody {...{ label, transitionId }} />
     </Link>
   )
