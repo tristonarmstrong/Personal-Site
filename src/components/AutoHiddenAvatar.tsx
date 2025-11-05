1f2da8637c1fd01d4970e35e7a34a94f69b74fef
Triston Armstrong <tarmstrong95@proton.me>
2025-11-04 23:46:13 -0700

    add element to keep avatar space when avatar moves

--- src/components/AutoHiddenAvatar.tsx
+++ src/components/AutoHiddenAvatar.tsx
@@ -4,7 +4,7 @@ import { Avatar } from "./Avatar"
 export function AutoHiddenAvatar({ handleNavigate }: { handleNavigate: (route: string) => () => void }) {
   const { state } = useFileRouter()
 
-  if (state.path.includes('blog')) return null
+  if (state.path.includes('blog')) return <this-element-takes-up-space />
 
   return (
     <Link style={"view-transition-name: nav"} to="/" onclick={handleNavigate("/")}><Avatar /></Link>
