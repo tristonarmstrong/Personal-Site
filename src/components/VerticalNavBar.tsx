11d3f3bfb41eaf6a248ed8db9f7c1d8e97cf9cd9
Triston Armstrong <tarmstrong95@proton.me>
2025-11-06 22:56:40 -0700

    update biome settings

--- src/components/VerticalNavBar.tsx
+++ src/components/VerticalNavBar.tsx
@@ -65,7 +65,27 @@ export function VerticalNavBar() {
 
 
 function HomeIcon() {
-  return <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-birdhouse-icon lucide-birdhouse"><path d="M12 18v4" /><path d="m17 18 1.956-11.468" /><path d="m3 8 7.82-5.615a2 2 0 0 1 2.36 0L21 8" /><path d="M4 18h16" /><path d="M7 18 5.044 6.532" /><circle cx="12" cy="10" r="2" /></svg>
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="48"
+      height="48"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="lucide lucide-birdhouse-icon lucide-birdhouse"
+    >
+      <path d="M12 18v4" />
+      <path d="m17 18 1.956-11.468" />
+      <path d="m3 8 7.82-5.615a2 2 0 0 1 2.36 0L21 8" />
+      <path d="M4 18h16" />
+      <path d="M7 18 5.044 6.532" />
+      <circle cx="12" cy="10" r="2" />
+    </svg>
+  )
 }
 function ExperienceIcon() {
   return <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-briefcase-business-icon lucide-briefcase-business"><path d="M12 12h.01" /><path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2" /><path d="M22 13a18.15 18.15 0 0 1-20 0" /><rect width="20" height="14" x="2" y="6" rx="2" /></svg>
