c0de8b0fbc8daa888568bcd5d8347aef83e39296
Triston Armstrong <tarmstrong95@proton.me>
2025-11-11 10:07:46 -0700

    fix a tag bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,10 +8,10 @@ export default function Home() {
 
       <section className="flex flex-col gap-4">
         <p>Software Developer with <span className={"yearthing"} title={"I started programming professionally in year 2020"}>{Math.abs(new Date().getFullYear() - 2020)} years</span> of experience, father to 3, and husband to 1 (not in that order) with a passion for building utility apps for myself to make my dev day-to-day easier. I also greatly enjoy,
-          {' '}<a className="text-yellow-500 underline" href={"https://automatetheboringstuff.com/"} target="_blank" rel="no-opener">automating the boring stuff</a> when the opertunity presents itself.</p>
+          {' '}<a className="text-yellow-500 underline" href={"https://automatetheboringstuff.com/"} target="_blank" rel="noopener">automating the boring stuff</a> when the opertunity presents itself.</p>
       </section>
 
-    
+
       <section className="flex flex-col gap-4 dark:bg-[#fff2] rounded-lg p-2 dark:opacity-50 border border-dashed border-[#0003] dark:border-[#fff3]">
         <small style={"color: var(--color-gray-400);"}>This is a "living site". I push half-baked chunks of UI to showcase its development in realtime, just... cause.. i find it interesting</small>
       </section>
