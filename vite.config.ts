7a6aa04aca954f9f6254e0e901e94a95ab0e54fb
Triston Armstrong <tarmstrong95@proton.me>
2025-11-13 09:21:33 -0700

    update configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import contentCollections from "@content-collections/vite";
-import { defineConfig } from "vite"
+import { defineConfig } from "vite";
 import kiru from "vite-plugin-kiru"
 
 export default defineConfig({
   plugins: [
+    contentCollections(),
     // @ts-ignore
     kiru({
       loggingEnabled: true,
       ssg: {
         transition: true
       },
-    }),
-    contentCollections()
+    })
   ],
   server: {
     watch: {
