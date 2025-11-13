87021be3d96d5b6e8b80d4aac4466d4b7f928d0c
Triston Armstrong <tarmstrong95@proton.me>
2025-11-13 00:21:00 -0700

    remove ssg temporarily

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,16 @@ export default defineConfig({
   plugins: [
     // @ts-ignore
     kiru({
-      ssg: {
-        dir: "./src/pages",
-        document: "document.tsx",
-        page: "index.{tsx,jsx}",
-        layout: "layout.{tsx,jsx}",
-        transition: true,
-        build: {
-          maxConcurrentRenders: 100
-        }
-      },
+      // ssg: {
+      //   dir: "./src/pages",
+      //   document: "document.tsx",
+      //   page: "index.{tsx,jsx}",
+      //   layout: "layout.{tsx,jsx}",
+      //   transition: true,
+      //   build: {
+      //     maxConcurrentRenders: 100
+      //   }
+      // },
     }),
     contentCollections()
   ],
