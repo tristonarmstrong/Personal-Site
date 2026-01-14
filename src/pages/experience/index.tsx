e3a6be815240a076bb4804be68225b2ee2ede45f
Triston Armstrong <tarmstrong95@proton.me>
2026-01-13 22:14:31 -0700

    fix ventra health end date

--- src/pages/experience/index.tsx
+++ src/pages/experience/index.tsx
@@ -3,7 +3,7 @@ export default function Experience() {
     <main className="text-sm mt-10 flex flex-col gap-4" style={"view-transition-name: homepage"}>
       <p>Companies I've worked at and maybe where I'm currently working if I can convince someone to give me money in exchange for some code.</p>
       <ul>
-        <Work href="https://ventrahealth.com/" comp="Ventra Health" time="2023 - Pres" />
+        <Work href="https://ventrahealth.com/" comp="Ventra Health" time="2023 - 2025" />
         <Work href="https://www.randstadusa.com/" comp="Randstad Technologies" time="2021 - 2023" />
         <Work href="https://damianoglobal.com/" comp="Damiano Global Corp." time="2021" />
         <Work href="" comp="Makers Ladder LLC" time="2020" />
