a8c71e7d764726fcdbaded3c3ac3720d296314a0
Triston Armstrong <tarmstrong95@proton.me>
2026-01-28 11:47:28 -0800

    add a little callout on experience page

--- src/pages/experience/index.tsx
+++ src/pages/experience/index.tsx
@@ -3,6 +3,7 @@ export default function Experience() {
     <main className="text-sm mt-10 flex flex-col gap-4" style={"view-transition-name: homepage"}>
       <p>Companies I've worked at and maybe where I'm currently working if I can convince someone to give me money in exchange for some code.</p>
       <ul>
+        <Work href="" comp="Your Company Here" time="Immediately" focus/>
         <Work href="https://ventrahealth.com/" comp="Ventra Health" time="2023 - 2025" />
         <Work href="https://www.randstadusa.com/" comp="Randstad Technologies" time="2021 - 2023" />
         <Work href="https://damianoglobal.com/" comp="Damiano Global Corp." time="2021" />
@@ -19,9 +20,13 @@ export default function Experience() {
   )
 }
 
-function Work({ comp, time, href }: { comp: string, time: string, href: string }) {
+function Work({ comp, time, href, focus=false }: { comp: string, time: string, href: string, focus: boolean }) {
+  const unmutedLink = "text-gray-700", mutedLink = "text-gray-400"
+  const isMutedLink = href ? unmutedLink : mutedLink
+  const isFocused = focus ? "animate-pulse" : ""
+
   return (
-    <a href={href} target={"_blank"} className={`${href ? "text-gray-400" : "text-gray-700"}`}>
+    <a href={href} target={"_blank"} className={`${isMutedLink} ${isFocused}`}>
       <li className={"flex w-full items-center gap-1"}>
         <span className={""}>{comp}</span>
         <span className={"h-full border border-dashed flex-1 border-gray-400"}></span>
