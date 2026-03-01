f1c36a5b960d9b2f529844e9c99e26378c3a8a64
Triston Armstrong <Tarmstrong95@proton.me>
2026-03-01 01:25:39 -0700

    add transition to file router

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { FileRouter } from "kiru/router"
 
 mount(<FileRouter config={{
   pages: import.meta.glob("/**/index.{tsx,jsx}"),
-  layouts: import.meta.glob("/**/layout.{tsx, jsx}")
+  layouts: import.meta.glob("/**/layout.{tsx, jsx}"),
+  transition: true
   // biome-ignore lint/style/noNonNullAssertion: cause im a bad bitch
 }} />, document.getElementById("app")!)
