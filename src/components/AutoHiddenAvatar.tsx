c68910492e0b244366d6fda933547edd15d91e81
Triston Armstrong <tarmstrong95@proton.me>
2026-02-28 22:26:25 -0700

    temp comment unused var

--- src/components/AutoHiddenAvatar.tsx
+++ src/components/AutoHiddenAvatar.tsx
@@ -1,8 +1,8 @@
-import { Link, useFileRouter } from "kiru/router"
+import { Link, /*useFileRouter */ } from "kiru/router"
 import { Avatar } from "./Avatar"
 
 export function AutoHiddenAvatar() {
-  const { state } = useFileRouter()
+  // const { state } = useFileRouter()
 
   //TODO state pathname includes does not exist in new kiru
   // if (state.pathname.includes('blog')) return <this-element-takes-up-space />
