ca29ddcff526dcf74648304867cdd572e70872f0
Triston Armstrong <tarmstrong95@proton.me>
2026-03-15 17:30:16 -0600

    🐛bug: fixed hydration mismatch in years experience calculation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,54 @@
-import { SEO } from "../components/SEO"
+import { signal } from "kiru";
+import { SEO } from "../components/SEO";
 
-export default function Home() {
-  return () => (
-    <main className="text-sm mt-10 flex flex-col gap-4" style={"view-transition-name: homepage"}>
-      <SEO />
-      <section>
-        <h1 className="text-lg text-yellow-500">Triston Armstrong</h1>
-        <p className="text-sm text-gray-400 italic">Senior Software Engineer</p>
-      </section>
+const yearsExperience = signal(5);
 
-      <section className="flex flex-col gap-4">
-        <p>Software Developer with <span className={"yearthing"} title={"I started programming professionally in year 2020"}>{Math.abs(new Date().getFullYear() - 2020)} years</span> of experience, father to 3, and husband to 1 (not in that order) with a passion for building utility apps for myself to make my dev day-to-day easier. I also greatly enjoy,
-          {' '}<a className="text-yellow-500 underline" href={"https://automatetheboringstuff.com/"} target="_blank" rel="noopener">automating the boring stuff</a> when the opertunity presents itself.</p>
-      </section>
-
-      <section className="flex flex-col gap-4 bg-[#fff2] rounded-lg p-2 opacity-50 border border-dashed border-[#fff3]">
-        <small style={"color: var(--color-gray-400);"}>This is a "living site". I push half-baked chunks of UI to showcase its development in realtime, just... cause.. i find it interesting</small>
-      </section>
-    </main>
-  )
+if (typeof window !== "undefined") {
+	yearsExperience.value = Math.abs(new Date().getFullYear() - 2020);
 }
 
+export default function Home() {
+	return () => (
+		<main
+			className="text-sm mt-10 flex flex-col gap-4"
+			style={"view-transition-name: homepage"}
+		>
+			<SEO />
+			<section>
+				<h1 className="text-lg text-yellow-500">Triston Armstrong</h1>
+				<p className="text-sm text-gray-400 italic">Senior Software Engineer</p>
+			</section>
+
+			<section className="flex flex-col gap-4">
+				<p>
+					Software Developer with{" "}
+					<span
+						className={"yearthing"}
+						title={"I started programming professionally in year 2020"}
+					>
+						{yearsExperience.value} years
+					</span>{" "}
+					of experience, father to 3, and husband to 1 (not in that order) with
+					a passion for building utility apps for myself to make my dev
+					day-to-day easier. I also greatly enjoy,{" "}
+					<a
+						className="text-yellow-500 underline"
+						href={"https://automatetheboringstuff.com/"}
+						target="_blank"
+						rel="noopener"
+					>
+						automating the boring stuff
+					</a>{" "}
+					when the opertunity presents itself.
+				</p>
+			</section>
+
+			<section className="flex flex-col gap-4 bg-[#fff2] rounded-lg p-2 opacity-50 border border-dashed border-[#fff3]">
+				<small style={"color: var(--color-gray-400);"}>
+					This is a "living site". I push half-baked chunks of UI to showcase
+					its development in realtime, just... cause.. i find it interesting
+				</small>
+			</section>
+		</main>
+	);
+}
