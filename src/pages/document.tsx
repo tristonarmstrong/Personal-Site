939dcca426b3f968bee0532773e89e350fa91337
Triston Armstrong <tarmstrong95@proton.me>
2026-06-11 15:17:25 -0600

    add link to manifest

--- src/pages/document.tsx
+++ src/pages/document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
 		    <meta charset="UTF-8"/>
 		    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <Head.Outlet />
+        <link rel="manifest" href="/manifest.json"/>
       </head>
       <Body.Outlet />
     </html>
